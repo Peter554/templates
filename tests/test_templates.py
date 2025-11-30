3e64d9e911e981fa5564edf8fab6c9d6a0e6d68a
Peter Byfield <peter.byfield@kraken.tech>
2025-11-30 17:35:30 +0100

    Improving

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -5,13 +5,17 @@ import copier
 
 def test_template_py(tmp_path):
     copier.run_copy(
-        "./py",
+        "./templates/py",
         tmp_path,
         data={
             "project_name": "foo",
             "project_slug": "foo",
             "description": "foo",
+            "python_version": "3.13",
+            "create_cli": True,
+            "git_init": False,
         },
+        unsafe=True,
     )
 
     assert (
